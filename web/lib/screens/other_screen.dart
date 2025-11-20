1b3b68263b8a8e12fd7cc9231b26fd406df0a618
RnD0mN3m <ktq2390306@stu.o-hara.ac.jp>
2025-11-20 09:39:10 +0900

    設定

--- web/lib/screens/other_screen.dart
+++ web/lib/screens/other_screen.dart
@@ -3,6 +3,8 @@ import '../bottom_nav.dart';
 import 'volume_settings_screen.dart';
 import 'contact_screen.dart';
 import 'language_settings_screen.dart';
+import 'purchase_screen.dart';
+import 'payment_management_screen.dart';
 import '../services/auth_api_service.dart';
 import '../services/token_storage_service.dart';
 import 'login_screen.dart';
@@ -209,14 +211,24 @@ class _OtherScreenState extends State<OtherScreen> {
               context,
               icon: Icons.payment,
               label: '支払い情報管理',
-              onTap: () {},
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (_) => const PaymentManagementScreen()),
+                );
+              },
             ),
             const SizedBox(height: 12),
             _buildMenuButton(
               context,
               icon: Icons.subscriptions,
               label: 'サブスク登録・解約',
-              onTap: () {},
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (_) => const PurchaseScreen()),
+                );
+              },
             ),
             const SizedBox(height: 12),
             _buildMenuButton(
